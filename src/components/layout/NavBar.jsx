556a88b21a6506a6b50d801b586a6eeb110ef938
Furkan <turkogluvelifurkan@gmail.com>
2022-11-21 02:21:09 +0300

    added hover

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -9,7 +9,10 @@ function Navbar({ title }) {
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
           <FaGithub className="inline pr-2 text-3xl" />
-          <Link to="/" className="text-lg font-bold align-middle">
+          <Link
+            to="/"
+            className="text-lg font-bold align-middle hover:bg-neutral-focus"
+          >
             {title}
           </Link>
         </div>
