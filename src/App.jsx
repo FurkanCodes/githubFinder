b72646d833e3f0470331e38f6fefdb739966df7e
Furkan <turkogluvelifurkan@gmail.com>
2022-12-07 00:52:51 +0300

    THEMES

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,37 @@ import Notfound from "./pages/Notfound";
 import User from "./components/users/User";
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
+import ThemeDropdown from "./ThemeDropdown";
+import { useContext } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("cyberpunk");
+
+  const handleClick = () => {
+    setTheme("dark");
+  };
+
+  const setCyberpunk = () => {
+    setTheme("cyberpunk");
+  };
+
+  const setRetro = () => {
+    setTheme("retro");
+  };
   return (
     <GithubProvider>
       <AlertProvider>
         <Router>
-          <Theme dataTheme="cyberpunk">
+          <Theme dataTheme={theme}>
             <div className="min-h-screen flex flex-col justify-between">
               <Navbar />
               <main className="container mx-auto px-3 pb-12 ">
                 <Alert />
+                <ThemeDropdown
+                  handleClick={handleClick}
+                  setCyberpunk={setCyberpunk}
+                  setRetro={setRetro}
+                />
                 <Routes>
                   <Route path="/" element={<Home />} />
                   <Route path="/about" element={<About />} />
